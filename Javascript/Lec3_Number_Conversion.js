9b8ad5573b3a21440f7d2a00df46363108da5c99
Sparsh Yadav <96286051+sparshyadav@users.noreply.github.com>
2024-07-19 12:24:02 +0530

    Lecture 3 Data Type Converstions

---
+++ Javascript/Lec3_Number_Conversion.js
@@ -0,0 +1,44 @@
+// let score = "22";
+// console.log(typeof score);
+
+// let converted = Number(score);
+// console.log(converted);
+// console.log(typeof converted); // Till here everything works fine
+
+// let a = "33bc";
+// let b = null;
+// let c = undefined;
+
+// let conA=Number(a);
+// let conB=Number(b);
+// let conC=Number(c);
+
+// console.log(typeof conA); // Number
+// console.log(conA); // NaN
+// console.log(typeof conB); // Number
+// console.log(conB); // 0
+// console.log(typeof conC); // Number
+// console.log(conC); // NaN
+
+
+// Boolean Conversion
+let a=1;
+let b=2;
+let c="";
+let d="Sparsh";
+
+let conA=Boolean(a);
+let conB=Boolean(b);
+let conC=Boolean(c);
+let conD=Boolean(d);
+
+console.log(conA); // True
+console.log(typeof conA); // Boolean
+console.log(conB); // True
+console.log(typeof conB); // Boolean
+console.log(conC); // False
+console.log(typeof conC); // Boolean
+console.log(conD); // True
+console.log(typeof conD); // Boolean
+
+
