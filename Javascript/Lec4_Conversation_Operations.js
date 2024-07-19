077b1ebaf3f8d8b56e2711a12dba05a36b1b5630
Sparsh Yadav <96286051+sparshyadav@users.noreply.github.com>
2024-07-19 12:56:18 +0530

    Converstion using Operators

--- Javascript/Lec4_Conversation_Operations.js
+++ Javascript/Lec4_Conversation_Operations.js
@@ -1,35 +1,41 @@
-// let str1 = "Hello";
-// let str2 = " Sparsh";
-// console.log(str1 + str2); // Hello Sparsh
+// // let str1 = "Hello";
+// // let str2 = " Sparsh";
+// // console.log(str1 + str2); // Hello Sparsh
 
-// console.log("1" + 2); // 12
-// console.log(1 + "2"); // 12
-// console.log("1" + 2 + 2); // 122
-// console.log(1 + 2 + "2"); // 32
-// console.log(1+2+"2"+3+3+"3"); // 32333
+// // console.log("1" + 2); // 12
+// // console.log(1 + "2"); // 12
+// // console.log("1" + 2 + 2); // 122
+// // console.log(1 + 2 + "2"); // 32
+// // console.log(1+2+"2"+3+3+"3"); // 32333
 
-// numeric string used with + gives string type
-let result;
+// // numeric string used with + gives string type
+// let result;
 
-// convert number to string
-result = "3" + 2; 
-console.log(result, "-", typeof(result));
+// // convert number to string
+// result = "3" + 2; 
+// console.log(result, "-", typeof(result));
 
-result = "3" + true; 
-console.log(result, "-", typeof(result));
+// result = "3" + true; 
+// console.log(result, "-", typeof(result));
 
-result = "3" + null; 
-console.log(result, "-", typeof(result));
+// result = "3" + null; 
+// console.log(result, "-", typeof(result));
 
-// convert string to Number
-result = "4" - "2"; 
-console.log(result, "-", typeof(result));
+// // convert string to Number
+// result = "4" - "2"; 
+// console.log(result, "-", typeof(result));
 
-result = "4" - 2;
-console.log(result, "-", typeof(result));
+// result = "4" - 2;
+// console.log(result, "-", typeof(result));
 
-result = "4" * 2;
-console.log(result, "-", typeof(result));
+// result = "4" * 2;
+// console.log(result, "-", typeof(result));
 
-result = "4" / 2;
-console.log(result, "-", typeof(result));
+// result = "4" / 2;
+// console.log(result, "-", typeof(result));
+
+const greet=function(){
+    console.log("Hello");
+}
+
+console.log(typeof greet);
