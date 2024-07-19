ee8ad594ea4f6d1876c9ad94062448b676b65715
Sparsh Yadav <96286051+sparshyadav@users.noreply.github.com>
2024-07-19 12:24:25 +0530

    Converstion using Operators

---
+++ Javascript/Lec4_Conversation_Operations.js
@@ -0,0 +1,9 @@
+let str1 = "Hello";
+let str2 = " Sparsh";
+console.log(str1 + str2); // Hello Sparsh
+
+console.log("1" + 2); // 12
+console.log(1 + "2"); // 12
+console.log("1" + 2 + 2); // 122
+console.log(1 + 2 + "2"); // 32
+console.log(1+2+"2"+3+3+"3"); // 32333
