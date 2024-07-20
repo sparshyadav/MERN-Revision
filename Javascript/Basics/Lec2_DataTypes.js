61f4f2268933aaf7d0e931c73f7626e08c006bdc
Sparsh Yadav <96286051+sparshyadav@users.noreply.github.com>
2024-07-20 11:10:07 +0530

    Basics of JavaScript Completed

---
+++ Javascript/Basics/Lec2_DataTypes.js
@@ -0,0 +1,24 @@
+let name = "Sparsh";
+let age = 22;
+let number = 81777444444444444222222222222227544426636733333333333332222222286845678743426556477776275;
+let isEmployed = true;
+let state;
+let married = null;
+
+console.table([typeof (name), typeof (age), typeof (number), typeof (isEmployed), typeof (state), typeof (married)]);
+
+// typeof(null) -> Object
+
+/* Primitive Data Types
+Number
+bigInt
+String
+Boolean
+NUll -> Standalone Value(empty)
+undefined -> When no value is given
+symbol -> Unique Values
+*/
+
+/* Non Primitive Data Types
+Object
+*/
---
+++ Javascript/Basics/Lec5_Date.js
@@ -0,0 +1,7 @@
+// let myDate=new Date();
+// console.log(myDate); // 2024-07-19T15:52:37.349Z
+// console.log(myDate.toString()); // Fri Jul 19 2024 21:22:37 GMT+0530 (India Standard Time)
+// console.log(typeof myDate); // Object
+
+let myCreatedDate = new Date(2024, 6, 19); // Year Month Date
+console.log(myCreatedDate.toDateString()); // Fri Jul 19 2024
