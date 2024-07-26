cc77cf36301eb6a331c6523e284b3dcbceaf272d
Sparsh Yadav <96286051+sparshyadav@users.noreply.github.com>
2024-07-26 12:57:47 +0530

    Selecting Elements Using DOM

---
+++ Javascript/DOM/script2.js
@@ -0,0 +1,16 @@
+// // DOM Manipulations
+
+// // const title=document.getElementById('main-heading');
+// // console.log(title);
+
+// // const listItems=document.getElementsByClassName("list_items");
+// // console.log(listItems);
+
+// // const title=document.getElementsByTagName("li");
+// // console.log(title);  
+
+// // const container=document.querySelector("div");
+// // console.log(container);
+
+// const container=document.querySelectorAll  ("div");
+// console.log(container);
