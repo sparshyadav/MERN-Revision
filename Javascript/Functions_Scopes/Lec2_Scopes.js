3adb4eb4d37b864d524799deafeb253ab2280251
Sparsh Yadav <96286051+sparshyadav@users.noreply.github.com>
2024-07-26 11:25:41 +0530

    Working on DOM

---
+++ Javascript/Functions_Scopes/Lec2_Scopes.js
@@ -0,0 +1,14 @@
+function one(){
+    const username="Sparsh";
+
+    function two(){
+        const website="google";
+        console.log(username);
+    }
+
+    // console.log(website);
+
+    two();
+}
+
+one();
