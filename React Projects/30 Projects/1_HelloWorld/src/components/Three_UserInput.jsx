6d4ca558e3b2bf1ae5ce376602bb0a6469e9f551
Sparsh Yadav <96286051+sparshyadav@users.noreply.github.com>
2024-09-05 17:40:00 +0530

    Project 3 Completed

--- React Projects/30 Projects/1_HelloWorld/src/components/Three_UserInput.jsx
+++ React Projects/30 Projects/1_HelloWorld/src/components/Three_UserInput.jsx
@@ -1,13 +1,21 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const Three_UserInput = () => {
+    const [username, setUserName] = useState("");
+    const [password, setPassowrd] = useState("");
     return (
         <div>
             <label htmlFor="">Username</label>
-            <input type="text" />
+            <input type="text" value={username} onChange={(e) => setUserName(e.target.value)} />
             <br></br>
             <label htmlFor="">Password</label>
-            <input type="text" />
+            <input type="text" value={password} onChange={(e) => setPassowrd(e.target.value)} />
+
+            <div className="div">
+                <h1>UserInput</h1>
+                <p>Username: {username}</p>
+                <p>Password: {password}</p>
+            </div>
         </div>
     )
 }
