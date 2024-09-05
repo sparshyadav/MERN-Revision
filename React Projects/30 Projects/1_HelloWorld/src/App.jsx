f7549f2641f698311033eccc62f86831e1dc0034
Sparsh Yadav <96286051+sparshyadav@users.noreply.github.com>
2024-09-05 17:46:46 +0530

    Project 4 Completed

--- React Projects/30 Projects/1_HelloWorld/src/App.jsx
+++ React Projects/30 Projects/1_HelloWorld/src/App.jsx
@@ -2,13 +2,15 @@ import React from 'react'
 import One_HelloWorld from './components/One_HelloWorld'
 import Two_Counter from './components/Two_Counter'
 import Three_UserInput from './components/Three_UserInput'
+import Four_DisplayList from './components/Four_DisplayList'
 
 const App = () => {
   return (
     <div>
       {/* <One_HelloWorld /> */}
       {/* <Two_Counter /> */}
-      <Three_UserInput />
+      {/* <Three_UserInput /> */}
+      <Four_DisplayList />
     </div>
   )
 }
--- React Projects/30 Projects/1_HelloWorld/src/components/Four_DisplayList.jsx
+++ React Projects/30 Projects/1_HelloWorld/src/components/Four_DisplayList.jsx
@@ -1,9 +1,14 @@
 import React from 'react'
 
 const Four_DisplayList = () => {
+    const items=["item1", "item2", "item3", "item4", "item5"];
   return (
     <div>
-      
+      <ul>
+        {items.map((item)=>(
+            <h1>{item}</h1>
+        ))}
+      </ul>
     </div>
   )
 }
